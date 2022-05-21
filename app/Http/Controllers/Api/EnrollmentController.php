8b6e9ac2b7b32d276ffe56df62d7a5a84c9a3c89
Bruce Rodriguez <bruce.rodriguez94@gmail.com>
2022-05-21 01:29:48 -0500

    Se agrego la ruta para que el usuario pueda listar sus cursos matriculados

--- app/Http/Controllers/Api/EnrollmentController.php
+++ app/Http/Controllers/Api/EnrollmentController.php
@@ -8,11 +8,11 @@ use App\Models\Enrollment;
 
 class EnrollmentController extends Controller
 {
-    public function search(Request $request)
+    public function search(int $userId)
     {
         $enrollments = Enrollment::selectRaw('courses.*, enrollments.id enrollment_id')
             ->join('courses', 'courses.id', 'enrollments.coruse_id')
-            ->where('user_id', $request->user_id)
+            ->where('user_id', $userId)
             ->get();
         return response()->services(true, 'Lista de cursos matriculados', $enrollments);
     }
